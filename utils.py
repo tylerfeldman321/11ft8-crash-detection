8305ba75ef93ef7c0a1391bfa40b9791bd6a863b
Tyler Feldman <tjf40@duke.edu>
2022-11-12 00:04:59 -0500

    Reduce number of decimal points printed for fps

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def load_video(filepath):
     width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps    = cap.get(cv2.CAP_PROP_FPS)
-    print(f'Loaded video from {filepath}, with {num_frames} frames, {width} width, {height} height, and {fps} fps')
+    print(f'Loaded video from {filepath}, with {num_frames} frames, {width} width, {height} height, and {fps:.3} fps')
     return cap
 
 
