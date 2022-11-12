14804b84d4f5c91826d1a2467578c1f3a6d661ff
Tyler Feldman <tjf40@duke.edu>
2022-11-11 22:15:42 -0500

    Print out video description when loading

--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_video(filepath):
     """ Load video as a cv2 VideoCapture given its filepath """
-    capture = cv2.VideoCapture(filepath)
-    return capture
+    print('fdsa')
+    cap = cv2.VideoCapture(filepath)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps    = cap.get(cv2.CAP_PROP_FPS)
+    print(f'Loaded video from {filepath}, with {num_frames} frames, {width} width, {height} height, and {fps} fps')
+    return cap
 
 
 def display_video(filepath, frame_shape=(960, 540)):
@@ -16,12 +23,26 @@ def display_video(filepath, frame_shape=(960, 540)):
         if frame is None:
             break
 
-        frame_resized = cv2.resize(frame, frame_shape)
-        gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_resized = cv2.resize(gray, frame_shape)
+        cv2.imshow('frame', frame_resized)
 
         if cv2.waitKey(1) == ord('q'):
             break
 
     capture.release()
     cv2.destroyAllWindows()
+
+
+def display_rectangle_of_image(image_path, upper_left=(335, 890), lower_left=(405, 1020)):
+    # In format row, col
+    image = cv2.imread(image_path)
+    image_rectangle = image[upper_left[0]:lower_left[0],upper_left[1]:lower_left[1],:]
+    plt.imshow(image_rectangle)
+    plt.show()
+    cv2.imwrite('image.png', image_rectangle)
+
+
+if __name__ == '__main__':
+    # display_video('data/20200103.082000.11foot82b.copy.mp4')
+    display_rectangle_of_image('data/sign_on_example.png')
