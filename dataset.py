b8165838a53f1678fe657afe9b9c3b74ea301f1c
Tyler Feldman <tylerfeldman321@gmail.com>
2022-12-05 14:35:07 -0500

    Add verbose argument when loading dataset

--- dataset.py
+++ dataset.py
@@ -12,7 +12,9 @@ BAR_SIM_RESULTS_CSV = os.path.join('data', 'ssim.csv')
 LABELS_CSV = os.path.join('data', 'labels.csv')
 
 
-def load_dataset():
+def load_dataset(verbose=True):
+
+    if verbose: print('Loading dataset...')
 
     # sound_data_df = pd.read_csv()  # TODO load sound data
     sign_detection = pd.read_csv(SIGN_DETECTION_RESULTS_CSV).to_numpy()
@@ -35,18 +37,16 @@ def load_dataset():
             X, y = np.hstack((X, video_data)), np.hstack((y, video_labels))
 
     X, y = X.T, y.T
-
-    print('--------------------------')
-    print(f'Loaded dataset with data of shape = {X.shape}, and labels of shape: {y.shape}')
-    print(f'Number of samples: {X.shape[0]}, number of features: {X.shape[1]}')
-    print(f'Number of true samples: {int(np.sum(y))}')
-    print('--------------------------')
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    print(f'Train Dataset: {X_train.shape}, Train Labels: {y_train.shape}, Num True Samples: {np.sum(y_train)}')
-    print(f'Test Dataset: {X_test.shape}, Test Labels: {y_test.shape}, Num True Samples: {np.sum(y_test)}')
-    print('--------------------------')
+    if verbose:
+        print('--------------------------')
+        print(f'Loaded dataset with data of shape = {X.shape}, and labels of shape: {y.shape}')
+        print(f'Number of samples: {X.shape[0]}, number of features: {X.shape[1]}')
+        print(f'Number of true samples: {int(np.sum(y))}')
+        print(f'Train Dataset: {X_train.shape}, Train Labels: {y_train.shape}, Num True Samples: {np.sum(y_train)}')
+        print(f'Test Dataset: {X_test.shape}, Test Labels: {y_test.shape}, Num True Samples: {np.sum(y_test)}')
+        print('--------------------------')
 
     return X_train, X_test, y_train, y_test
 
