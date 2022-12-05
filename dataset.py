b3c243dbb84c0fbda9cb2903103601f0791f5323
Tyler Feldman <tylerfeldman321@gmail.com>
2022-12-05 14:07:43 -0500

    Split into train and test sets

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import glob
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 # SOUND_DATA_CSV = os.path.join()  # TODO path to csv with sound data
@@ -18,23 +19,36 @@ def load_dataset():
     ssim = pd.read_csv(BAR_SIM_RESULTS_CSV).to_numpy()
     labels = pd.read_csv(LABELS_CSV).to_numpy()
 
-    X, Y = None, None
+    X, y = None, None
 
-    for col_idx in range(labels.shape[1]):
+    for col_idx in range(1, labels.shape[1]):
+
         video_data = np.vstack((sign_detection[:, col_idx],
                               ssim[:, col_idx]))  # TODO: add in sound data
         video_labels = labels[:, col_idx]
 
         # TODO Do processing on data here?
 
-        if X is None or Y is None:
-            X, Y = video_data, video_labels
+        if X is None or y is None:
+            X, y = video_data, video_labels
         else:
-            X, Y = np.hstack((X, video_data)), np.hstack((Y, video_labels))
+            X, y = np.hstack((X, video_data)), np.hstack((y, video_labels))
 
-    print(f'Loaded dataset with data of shape = {X.shape}, and labels of shape: {Y.shape}')
+    X, y = X.T, y.T
 
-    return X, Y
+    print('--------------------------')
+    print(f'Loaded dataset with data of shape = {X.shape}, and labels of shape: {y.shape}')
+    print(f'Number of samples: {X.shape[0]}, number of features: {X.shape[1]}')
+    print(f'Number of true samples: {int(np.sum(y))}')
+    print('--------------------------')
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    print(f'Train Dataset: {X_train.shape}, Train Labels: {y_train.shape}, Num True Samples: {np.sum(y_train)}')
+    print(f'Test Dataset: {X_test.shape}, Test Labels: {y_test.shape}, Num True Samples: {np.sum(y_test)}')
+    print('--------------------------')
+
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == '__main__':
