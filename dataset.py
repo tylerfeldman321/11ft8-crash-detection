cf0fc77ac5b32e82de3b35516ab51f764e54e0fa
Tyler Feldman <tylerfeldman321@gmail.com>
2022-12-05 17:23:38 -0500

    Move around print statements

--- dataset.py
+++ dataset.py
@@ -37,21 +37,23 @@ def load_dataset(verbose=True, show_results=True):
         if show_results:
             plot_features_for_video(video_name, ssim_results, sign_detection_results, variance, video_labels)
 
-        video_data = np.vstack((variance, ssim_results))  # TODO: add in sound data
+        video_data = np.vstack((variance, ssim_results))  # np.expand_dims(variance, axis=0)  # TODO: add in sound data
 
         if X is None or y is None:
             X, y = video_data, video_labels
         else:
             X, y = np.hstack((X, video_data)), np.hstack((y, video_labels))
 
-    X, y = X.T, y.T
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
     if verbose:
         print('--------------------------')
         print(f'Loaded dataset with data of shape = {X.shape}, and labels of shape: {y.shape}')
         print(f'Number of samples: {X.shape[0]}, number of features: {X.shape[1]}')
         print(f'Number of true samples: {int(np.sum(y))}')
+
+    X, y = X.T, y.T
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    if verbose:
         print(f'Train Dataset: {X_train.shape}, Train Labels: {y_train.shape}, Num True Samples: {np.sum(y_train)}')
         print(f'Test Dataset: {X_test.shape}, Test Labels: {y_test.shape}, Num True Samples: {np.sum(y_test)}')
         print('--------------------------')
