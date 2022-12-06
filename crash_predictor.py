Fix sign detection video processing; add generator and csv for sign detection variance

crash_predictor.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
-from dataset import load_dataset
+from dataset import load_dataset, plot_features_for_video
 import time
 import pickle
 import os
@@ -144,11 +144,13 @@ class CrashPredictor:
         print('Predicted Crashes: ', end='')
         print(*times)
 
-    def extract_features(self, video_file_path):
+    def extract_features(self, video_file_path, show=False):
         audio_data = get_normalized_audio_amplitude(video_file_path, os.path.splitext(os.path.basename(video_file_path))[0])
-        template_matching_variance = SignDetector('data/sign_on_template.png').process_video(video_file_path)
+        template_matching_variance = SignDetector().process_video(video_file_path, compute_variance=True)
         ssim, fps = CrashBarSSIM().detect(video_file_path)
         data = np.stack((template_matching_variance, ssim, audio_data), axis=1)
+        if show:
+            plot_features_for_video(video_file_path, ssim_results=ssim, variance=template_matching_variance, sound_data=audio_data)
         return data
 
 

dataset.py
@@ -9,7 +9,7 @@ from sign_detection.sign_detector import extract_variance_of_moving_window
 
 
 SOUND_DATA_CSV = os.path.join('data', 'audio.csv')
-SIGN_DETECTION_RESULTS_CSV = os.path.join('data', 'sign_detection_results.csv')
+SIGN_DETECTION_RESULTS_CSV = os.path.join('data', 'sign_detection_variance.csv')
 BAR_SIM_RESULTS_CSV = os.path.join('data', 'ssim.csv')
 LABELS_CSV = os.path.join('data', 'labels.csv')
 """
@@ -30,7 +30,7 @@ def load_dataset(verbose=True, show_results=True):
     np.random.shuffle(video_names)
 
     sound_df = pd.read_csv(SOUND_DATA_CSV)
-    sign_detection_df = pd.read_csv(SIGN_DETECTION_RESULTS_CSV)
+    sign_detection_variance_df = pd.read_csv(SIGN_DETECTION_RESULTS_CSV)
     ssim_df = pd.read_csv(BAR_SIM_RESULTS_CSV)
     labels_df = pd.read_csv(LABELS_CSV)
 
@@ -40,16 +40,14 @@ def load_dataset(verbose=True, show_results=True):
     test_data_names = []
 
     for video_name in video_names:
-        sign_detection_results = sign_detection_df[video_name].values
-        variance = extract_variance_of_moving_window(sign_detection_results)
+        variance = sign_detection_variance_df[video_name].values
         ssim_results = ssim_df[video_name].values
         video_labels = labels_df[video_name].values
         sound = sound_df[video_name].values
         sound = sound / np.max(sound)
 
         if show_results:
-            plot_features_for_video(video_name, ssim_results,
-                                    sign_detection_results, variance, sound, video_labels)
+            plot_features_for_video(video_name=video_name, ssim_results=ssim_results, sign_detection_results=None, variance=variance, sound_data=sound, video_labels=video_labels)
 
         video_data = np.vstack((variance, ssim_results, sound))
         if num_train_videos > 0:
@@ -78,15 +76,20 @@ def load_dataset(verbose=True, show_results=True):
     return X_train, X_test, y_train, y_test, test_data_lengths, test_data_names
 
 
-def plot_features_for_video(video_name, ssim_results, sign_detection_results, variance, sound_data, video_labels):
+def plot_features_for_video(video_name, ssim_results=None, sign_detection_results=None, variance=None, sound_data=None, video_labels=None):
     plt.figure()
     plt.title(f'Features vs. Frame Number for {video_name}')
-    plt.plot(np.arange(0, len(ssim_results)), ssim_results, 'k-', label='Bar Similarity')
-    plt.plot(np.arange(0, len(sign_detection_results)),
-             sign_detection_results, 'b-', label='Template Matching')
-    plt.plot(np.arange(0, len(variance)), variance, 'r-', label='Variance')
-    plt.plot(np.arange(0, len(sound_data)), sound_data, 'c-', label='Sound')
-    plt.plot(np.arange(0, len(video_labels)), video_labels, 'g-', label='Label')
+    if ssim_results is not None: 
+        plt.plot(np.arange(0, len(ssim_results)), ssim_results, 'k-', label='Bar Similarity')
+    if sign_detection_results is not None:
+        plt.plot(np.arange(0, len(sign_detection_results)),
+                sign_detection_results, 'b-', label='Template Matching')
+    if variance is not None:
+        plt.plot(np.arange(0, len(variance)), variance, 'r-', label='Variance')
+    if sound_data is not None:
+        plt.plot(np.arange(0, len(sound_data)), sound_data, 'c-', label='Sound')
+    if video_labels is not None:
+        plt.plot(np.arange(0, len(video_labels)), video_labels, 'g-', label='Label')
     plt.xlabel('Frame')
     plt.ylabel('Feature Value')
     plt.legend(loc='best')

sign_detection/sign_detector.py
@@ -9,7 +9,7 @@ DATA_DIR = '../data'
 RESULTS_DIR = 'results'
 LABELS_DIR = 'labels'
 TEMPLATE_DIR = 'templates'
-TEMPLATE_FILEPATH = os.path.join(DATA_DIR, 'sign_on_template.png') # os.path.join(DATA_DIR, 'sign_on_template.png')
+TEMPLATE_FILEPATH = os.path.join('sign_detection', TEMPLATE_DIR, 'sign_on_template.png')
 
 TEMPLATE_MATCHING_METHODS = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
                              cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
@@ -22,11 +22,8 @@ class SignDetector:
     X_MIN = 750
     X_MAX = 1200
 
-    def __init__(self, template_path=None):
-        if template_path is None:
-            self.template = self._load_template()
-        else:
-            self.template = self._load_template(template_path)
+    def __init__(self):
+        self.template = self._load_template(os.path.join(os.getcwd(), TEMPLATE_FILEPATH))
 
     def is_sign_on(self, img):
         max_val, max_loc = self.template_match(img)
@@ -40,13 +37,13 @@ class SignDetector:
             self._display_template_match_result(img, res, min_loc, max_loc, method, w, h)
         return max_val, max_loc
 
-    def process_video(self, video_filepath, skip=5):
+    def process_video(self, video_filepath, compute_variance=True, skip=5):
         capture = cv2.VideoCapture(video_filepath)
         num_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        template_match_values = np.zeros(num_frames)
+        template_match_values = []
 
-        for i in range(num_frames):
+        for i in tqdm(range(num_frames), f'Processing video: '):
             ret, frame = capture.read()
             if frame is None:
                 break
@@ -56,12 +53,18 @@ class SignDetector:
                 gray_frame = self.get_roi_of_frame(gray_frame)
                 max_val, max_loc = self.template_match(gray_frame)
 
-                template_match_values[i] = max_val
+                template_match_values.append(max_val)
+
+        template_match_values = np.array(template_match_values)
 
         capture.release()
         template_match_values = clean_and_pad_sign_detection_results(template_match_values)
-        variance = extract_variance_of_moving_window(template_match_values)
-        return variance
+        
+        if compute_variance:
+            variance = extract_variance_of_moving_window(template_match_values)
+            return variance
+        else:
+            return template_match_values
 
     def _display_template_match_result(self, img, res, min_loc, max_loc, method, w, h):
         """ Display results of template matching """