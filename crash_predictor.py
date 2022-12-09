d7bc327f8ef1d93de893e749685ac2b2671b93ac
Tyler Feldman <tylerfeldman321@gmail.com>
2022-12-09 16:03:40 -0500

    Add docstrings

--- crash_predictor.py
+++ crash_predictor.py
@@ -26,7 +26,13 @@ class CrashPredictor:
             self.clf = MLPClassifier(max_iter=500, learning_rate='adaptive')
 
     def train(self, dataset, verbose=False, train_all=False):
-        """ Train the model on a set of extracted features """
+        """ Train the model on the training dataset
+
+        Args:
+            dataset (Tuple): Dataset returned from dataset.load_dataset
+            verbose (bool, optional): Whether to print details. Defaults to False.
+            train_all (bool, optional): Whether to train with all of the data instead of just the training set. Defaults to False.
+        """
         if verbose:
             print('Training model...')
 
@@ -40,11 +46,23 @@ class CrashPredictor:
             self.clf.fit(X_train, y_train)
 
     def save_model(self, save_path=os.path.join(MODELS_DIR, 'mlp.pkl'), verbose=True):
+        """ Saves the internal model in pickle format
+
+        Args:
+            save_path (str, optional): Path to where to save the model. Defaults to os.path.join(MODELS_DIR, 'mlp.pkl').
+            verbose (bool, optional): Whether to print out details. Defaults to True.
+        """
         pickle.dump(self.clf, open(save_path, 'wb'))
         if verbose:
             print(f'Saved model to {save_path}')
 
     def load_model(self, model_path=os.path.join(MODELS_DIR, 'mlp.pkl'), verbose=True):
+        """ Loads a model from the given model_path
+
+        Args:
+            model_path (str, optional): Path to the model pkl. Defaults to os.path.join(MODELS_DIR, 'mlp.pkl').
+            verbose (bool, optional): Whether to print out details. Defaults to True.
+        """
         self.clf = pickle.load(open(model_path, 'rb'))
         if verbose:
             print(f'Loaded model from {model_path}')
@@ -81,7 +99,7 @@ class CrashPredictor:
             len_pos_samples += len(positive_samples)
 
             if show_results:
-                self.plot_predictions(predictions, y, name)
+                self.plot_predictions(predictions, probabilities, y, name)
 
             frames_predictions = self.filter_raw_predictions(predictions, probabilities)
             timestamps = [self.convert_frame_to_timestamp(frame) for frame in frames_predictions]
@@ -102,7 +120,15 @@ class CrashPredictor:
         return frame_prediction_dict
 
     def predict(self, X, probability_threshold):
-        """ Make predictions on data X given probability threshold. Returns predictions (0 or 1) as well as probabilities """
+        """ Make predictions on data X given probability threshold. Returns predictions (0 or 1) as well as probabilities
+
+        Args:
+            X (ndarray): Numpy array of data. Shape should be (num_samples, num_features)
+            probability_threshold (float): Probability threshold above which to declare a frame as a crash.
+
+        Returns:
+            Tuple: Predictions (0 or 1) for each frame, and probability of crash for each frame. Both numpy arrays of shape (num_samples,)
+        """
         probabilities = self.clf.predict_proba(X)[:, 1]
         predictions = (probabilities >= probability_threshold) * 1
         return predictions, probabilities
@@ -114,7 +140,15 @@ class CrashPredictor:
         return split_x, split_y
 
     def filter_raw_predictions(self, predictions, probabilities):
-        """ Generate timestamps from predictions. Filter out predictions that are close to one another """        
+        """ Generate timestamps from predictions. Filter out predictions that are close to one another
+
+        Args:
+            predictions (ndarray): Numpy array of shape (num_samples,) that has predictions (0 or 1) for each frame
+            probabilities (ndarray): Numpy array of shape (num_samples,) that has probability of crash for each frame
+
+        Returns:
+            List: Filtered frames
+        """
         frame_predictions = np.where(predictions == 1.0)[0]
         frame_prediction_probabilities = probabilities[frame_predictions]
 
@@ -142,19 +176,43 @@ class CrashPredictor:
         return frame_predictions_filtered
 
     def convert_frame_to_timestamp(self, frame_number, fps=15):
-        """ Convert frame number to timestamp in seconds """
+        """ Convert frame to timestamp
+
+        Args:
+            frame_number (int): Frame number
+            fps (int, optional): Frames per second for the video. Defaults to 15.
+
+        Returns:
+            float: Time in seconds of the frame
+        """
         return frame_number/fps
 
     def convert_to_hour_minute_second(self, timestamps):
-        """ Convert List of timestamps (in seconds) to hour:minute:second format """
+        """ Convert a list of seconds to a list of times in hour, minutes, seconds format
+
+        Args:
+            timestamps (List): List of timestamps in seconds
+
+        Returns:
+            List: Times in hours:minutes:seconds format
+        """
         times = [time.strftime('%H:%M:%S', time.gmtime(timestamp)) for timestamp in timestamps]
         return times
 
-    def plot_predictions(self, predictions, labels, name):
-        """ Plot model predictions and labels """
+    def plot_predictions(self, predictions, probabilities, labels, name):
+        """ Plot predictions from the model
+
+        Args:
+            predictions (ndarray): 1D numpy array of predictions (0 or 1) for every frame
+            probabilities (ndarray): 1D numpy array of probabilities of crash for every frame
+            labels (ndarray): 1D numpy array of labels (0 or 1) for every frame
+            name (str): Name of the video file
+        """
+        print(predictions, labels)
         plt.figure()
         plt.title(f'Prediction results for {name}')
         plt.plot(np.arange(0, len(predictions)), predictions, label='predictions')
+        plt.plot(np.arange(0, len(probabilities)), probabilities, label='probability of crash')
         plt.plot(np.arange(0, len(labels)), labels, label='labels', zorder=10)
         plt.legend()
         plt.xlabel('Frame')
@@ -162,7 +220,15 @@ class CrashPredictor:
         plt.show()
 
     def __call__(self, args):
-        """ Run inference on a video file """
+        """ Run inference on a video path
+
+        Args:
+            args (Object): Object with a file and probability_threshold attributes.
+
+        Raises:
+            Exception: If provided video file doesn't exist
+            Exception: If the provided file is not in mp4 format
+        """
         video_file_path = args.file
         if not os.path.exists(video_file_path):
             raise Exception(f'Provided video does not exist: {video_file_path}')
@@ -170,6 +236,9 @@ class CrashPredictor:
             raise Exception('Provided file is not in mp4 format.')
 
         probability_threshold = args.probability_threshold
+
+        if not 0.0 <= probability_threshold <= 1.0:
+            raise Exception(f'Probability threshold must be between 0.0 and 1.0. Provided probability threshold: {probability_threshold}')
 
         capture = cv2.VideoCapture(video_file_path)
         fps = int(capture.get(cv2.CAP_PROP_FPS))
@@ -185,7 +254,15 @@ class CrashPredictor:
         print(*times)
 
     def extract_features(self, video_file_path, show=False):
-        """ Extract features from a video file """
+        """ Extract features from a video
+
+        Args:
+            video_file_path (str): Path to the video file
+            show (bool, optional): Whether to plot the extracted features. Defaults to False.
+
+        Returns:
+            ndarray: Numpy array of shape (num_samples, num_features)
+        """
         # TODO: multiprocessing?
         audio_data = get_normalized_audio_amplitude(
             video_file_path, os.path.splitext(os.path.basename(video_file_path))[0])
@@ -201,9 +278,11 @@ class CrashPredictor:
     def evaluate_performance(self, dataset, probability_threshold=DEFAULT_PROBABILITY_THRESHOLD, verbose=False):
         """ Evaluate the performance of the model
 
+        This assumes only 1 true crash in the video. If there are more than 1 true crashes in the video, then this function should be updated.
+
         Args:
-            dataset (_type_): _description_
-            probability_threshold (float, optional): Threshold above which to declare . Defaults to 0.5.
+            dataset (Tuple): dataset returned from load_data.
+            probability_threshold (float, optional): Probability threshold above which to declare a prediction as a true crash. Defaults to 0.5.
             verbose (bool, optional): Whether to print out detailed results. Defaults to False.
 
         Returns:
@@ -242,7 +321,7 @@ class CrashPredictor:
         """ Finds if we've found the true crash in the video
 
         Args:
-            pred_crash_frames (List): _description_
+            pred_crash_frames (List): List of predicted frames within the video
             true_crash_frame (int): Frame on which the crash occurs
 
         Returns:
